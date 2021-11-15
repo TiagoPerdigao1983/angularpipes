68969044fe855899d75f26e25f90c2be59db70d0
Tiago Perdigao <tiago.perdigao@hotmail.com>
2021-11-15 12:31:44 +0100

    pipe app finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,43 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  name: string = '';
+  date: string = '';
+  amount: number = 0;
+  height: number = 0;
+  miles: number = 0;
+
+  car = {
+    make: 'Seat',
+    model: 'Ibiza',
+    year: 2018,
+  };
+
+  onMilesChange(target: EventTarget) {
+    const value = (<HTMLInputElement>target).value;
+    this.miles = parseFloat(value);
+  }
+
+  onHeightChange(target: EventTarget) {
+    const value = (<HTMLInputElement>target).value;
+    this.height = parseFloat(value);
+  }
+
+  onNameChange(target: EventTarget) {
+    const value = (<HTMLInputElement>target).value;
+    this.name = value;
+  }
+
+  onDateChange(target: EventTarget) {
+    const value = (<HTMLInputElement>target).value;
+    this.date = value;
+  }
+
+  onAmountChange(target: EventTarget) {
+    const value = (<HTMLInputElement>target).value;
+    this.amount = parseFloat(value);
+  }
 }
